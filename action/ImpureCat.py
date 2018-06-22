d83e09e99079f7af21b8eda3d6b0622aef99d28e
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-06-22 19:22:28 +0800

    +action/ImpureCat

---
+++ action/ImpureCat.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__))+"/../"))
+import json
+import configparser
+from Kamisu66 import *
+import time
+import urllib.request
+from ImpureCat_config import delconfigs
+
+EC = EthicsCommittee("ImpureCat", "ImpureCat")
+
+dellimit = int(time.time()-86400*2)
+
+EC.cur.execute("""DELETE FROM `EC_message` WHERE `date` < %s""", (int(time.time()-86400*14)))
+EC.db.commit()
+
+for delconfig in delconfigs:
+	chat_id = delconfig[0]
+	deltime = int(time.time()-delconfig[1])
+	print(chat_id, deltime)
+	EC.cur.execute("""SELECT * FROM `EC_message` WHERE `chat_id` = %s AND `date` < %s AND `date` > %s AND `deleted` = 0 ORDER BY `date`""", (chat_id, str(deltime), str(dellimit)))
+	rows = EC.cur.fetchall()
+	for row in rows:
+		message_id = row[2]
+		print(row, message_id)
+		EC.deletemessage(chat_id, message_id)
