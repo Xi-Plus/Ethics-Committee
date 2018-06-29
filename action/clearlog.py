be945b29eb0b896da6b1e92e06270cdd269c5653
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-06-29 22:20:13 +0800

    Move clearlog to action and add config file

---
+++ action/clearlog.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__))+"/../"))
+import datetime
+import time
+from Kamisu66 import *
+from clearlog_config import limit
+
+EC = EthicsCommittee("", "")
+
+print(limit)
+timestamp = datetime.datetime.fromtimestamp(int(time.time()-limit)).strftime('%Y-%m-%d %H:%M:%S')
+
+rows = EC.cur.execute("""DELETE FROM EC_log WHERE `time` < %s""", (timestamp))
+EC.db.commit()
+print(f"delete {rows} rows")
