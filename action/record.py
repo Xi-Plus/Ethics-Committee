3159843a5e2a9aee35bf99cf3f7c3ebf96499acf
Xi-Plus <huangxuanyuxiplus@gmail.com>
2018-09-07 11:05:30 +0800

    record channel_post and ignore edited_message

--- action/record.py
+++ action/record.py
@@ -117,8 +117,35 @@ def main(data):
                               reply_to_user_id)
         except Exception as e:
             traceback.print_exc()
-            EC.log(traceback.format_exc())
+            EC.log("[record] "+traceback.format_exc())
+
+    elif "edited_message" in data:
+        pass
+
+    elif "channel_post" in data:
+        message = data["channel_post"]
+        chat_id = message["chat"]["id"]
+        user_id = ""
+        EC = EthicsCommittee(chat_id, user_id)
+        first_name = ""
+        message_id = message["message_id"]
+        reply_to_message_id = ""
+        reply_to_user_id = ""
+        if "reply_to_message" in message:
+            reply_to_message = message["reply_to_message"]
+            reply_to_message_id = reply_to_message["message_id"]
+        date = message["date"]
+        try:
+            if "text" in message:
+                EC.addmessage(user_id, message_id, first_name, "text",
+                              message["text"], date, reply_to_message_id,
+                              reply_to_user_id)
+            else:
+                EC.log("[record] "+json.dumps(data))
+        except Exception as e:
+            traceback.print_exc()
+            EC.log("[record] "+traceback.format_exc())
 
     else:
         EC = EthicsCommittee("unknown", "unknown")
-        EC.log(json.dumps(data))
+        EC.log("[record] "+json.dumps(data))
