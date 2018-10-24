9e2e84ce06e3361a59feee1ecb3d6daefbc0be96
xiplus <huangxuanyuxiplus@gmail.com>
2018-10-25 00:12:44 +0800

    Update action/change_invite_link.py

---
+++ action/change_invite_link.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__))+"/../"))
+import json
+import configparser
+from Kamisu66 import *
+import time
+import urllib.request
+from change_invite_link_config import *
+
+EC = EthicsCommittee("ChangeInviteLink", "ChangeInviteLink")
+
+if len(sys.argv) < 2:
+	exit("not given chat_id")
+
+try:
+	chat_id = int(sys.argv[1])
+except ValueError as e:
+	exit("chat_id not a value")
+
+if chat_id not in change_groups:
+	exit("chat_id not found")
+
+group = change_groups[chat_id]
+
+print(group)
+
+url = "https://api.telegram.org/bot"+EC.token+"/exportChatInviteLink?chat_id="+str(chat_id)
+
+try:
+	res = urllib.request.urlopen(url).read().decode("utf8")
+except urllib.error.HTTPError as e:
+	exit(e.read().decode("utf8"))
+
+res = json.loads(res)
+
+link = res["result"]
+
+EC.editmessage(group["message"]["message_id"], group["message"]["text"].format(link), chat_id=group["message"]["chat_id"])
