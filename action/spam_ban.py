3ccdf04f779ac815cc678dee48c9bd44ca0d0ec5
xiplus <huangxuanyuxiplus@gmail.com>
2018-11-06 08:48:46 +0800

    Update spam_ban, Split ban and warn regex

--- action/spam_ban.py
+++ action/spam_ban.py
@@ -17,17 +17,21 @@ def main(data):
         if chat_id not in (ban_username_chat+warn_username_chat+ban_text_chat+warn_text_chat):
             return
 
+        mode = []
         if "text" in message:
             text = message["text"]
-            mode = "text"
+            mode.append("text")
         elif "caption" in message:
             text = message["caption"]
-            mode = "text"
+            mode.append("text")
         elif "new_chat_member" in message:
             text = message["from"]["first_name"]
             if "last_name" in message["from"]:
                 text += " " + message["from"]["last_name"]
-            mode = "username"
+            mode.append("username")
+        elif "forward_from_chat" in message:
+            text = ""
+            mode.append("forward")
         else:
             return
 
@@ -35,7 +39,7 @@ def main(data):
         message_id = message["message_id"]
         date = message["date"]
         try:
-            if mode == "text" and re.match(r"/globalban@Kamisu66EthicsCommitteeBot", text):
+            if "text" in mode and re.match(r"/globalban@Kamisu66EthicsCommitteeBot", text):
                 if user_id in globalbanuser:
                     m = re.match(r"/globalban@Kamisu66EthicsCommitteeBot (\d+)(?:\n(.+))?", text)
                     ban_user_id = ""
@@ -54,6 +58,7 @@ def main(data):
 
                         EC.cur.execute("""SELECT `chat_id`, `message_id`, `type` FROM `EC_message` WHERE `user_id` = %s AND `date` > %s""", (ban_user_id, int(time.time()-3600)))
                         rows = EC.cur.fetchall()
+                        EC.log("[spam_ban] find {} messages to delete".format(len(rows)))
                         for row in rows:
                             if int(row[1]) in ban_username_chat:
                                 EC.log("[spam_ban] delete {} ({}) in {}".format(row[1], row[2], row[0]))
@@ -74,51 +79,52 @@ def main(data):
                         EC.sendmessage(chat_id=log_chat_id, message=message, parse_mode="HTML")
                 else:
                     EC.log("[spam_ban] {} /globalban not premission".format(user_id))
-            elif re.search(bad_regex, text):
-                if mode == "text":
-                    if chat_id in ban_text_chat:
-                        EC.cur.execute("""SELECT COUNT(*) FROM `EC_message` WHERE `user_id` = %s""", (user_id))
-                        cnt = int(EC.cur.fetchall()[0][0])
-                        if cnt < 5:
-                            EC.log("[spam_ban] kick {} in {} {}".format(user_id, chat_id, text))
+            if "forward" in mode:
+                EC.cur.execute("""SELECT COUNT(*) FROM `EC_message` WHERE `user_id` = %s""", (user_id))
+                cnt = int(EC.cur.fetchall()[0][0])
+                if cnt < 5:
+                    EC.sendmessage("@xiplus", reply=message_id)
+            if "text" in mode:
+                EC.cur.execute("""SELECT COUNT(*) FROM `EC_message` WHERE `chat_id` = %s AND `user_id` = %s""", (chat_id, user_id))
+                cnt = int(EC.cur.fetchall()[0][0])
+                if cnt < 5 or chat_id in [groups['xiplustestbot']]:
+                    if chat_id in ban_text_chat and re.search(ban_text_regex, text):
+                        EC.log("[spam_ban] kick {} in {} {}".format(user_id, chat_id, text))
 
-                            url = "https://api.telegram.org/bot"+EC.token+"/kickChatMember?chat_id="+str(chat_id)+"&user_id="+str(user_id)+"&until_date="+str(int(time.time()+86400*7))
-                            subprocess.Popen(['curl', '-s', url])
+                        url = "https://api.telegram.org/bot"+EC.token+"/kickChatMember?chat_id="+str(chat_id)+"&user_id="+str(user_id)+"&until_date="+str(int(time.time()+86400*7))
+                        subprocess.Popen(['curl', '-s', url])
 
-                            message = '#封 #自動 {0} ECbot banned <a href="tg://user?id={1}">{1}</a>\n理由：宣傳文字'.format(
-                                    groups_name[chat_id],
-                                    user_id
-                                )
-                            EC.log("[spam_ban] message {}".format(message))
-                            EC.sendmessage(chat_id=log_chat_id, message=message, parse_mode="HTML")
-
-                    elif chat_id in warn_text_chat:
-                        EC.cur.execute("""SELECT COUNT(*) FROM `EC_message` WHERE `chat_id` = %s AND `user_id` = %s""", (chat_id, user_id))
-                        cnt = int(EC.cur.fetchall()[0][0])
-                        if cnt < 5 or chat_id in [groups['xiplustestbot']]:
-                            EC.log("[spam_ban] warn {} in {} {}".format(user_id, chat_id, text))
-                            EC.sendmessage(warn_text, reply=message_id)
-
-                elif mode == "username":
-                    if chat_id in ban_username_chat:
-                        EC.deletemessage(chat_id, message_id)
-
-                        for ban_chat_id in ban_username_chat:
-                            EC.log("[spam_ban] kick {} in {} {}".format(user_id, ban_chat_id, text))
-
-                            url = "https://api.telegram.org/bot"+EC.token+"/kickChatMember?chat_id="+str(ban_chat_id)+"&user_id="+str(user_id)+"&until_date="+str(int(time.time()+86400*7))
-                            subprocess.Popen(['curl', '-s', url])
-
-                        message = '#封 #自動 所有群組(from {0}) ECbot banned <a href="tg://user?id={1}">{1}</a>\n理由：宣傳性用戶名'.format(
+                        message = '#封 #自動 {0} ECbot banned <a href="tg://user?id={1}">{1}</a>\n理由：宣傳文字'.format(
                                 groups_name[chat_id],
                                 user_id
                             )
                         EC.log("[spam_ban] message {}".format(message))
                         EC.sendmessage(chat_id=log_chat_id, message=message, parse_mode="HTML")
 
-                    elif chat_id in warn_username_chat:
+                    elif chat_id in warn_text_chat and re.search(warn_text_regex, text):
                         EC.log("[spam_ban] warn {} in {} {}".format(user_id, chat_id, text))
                         EC.sendmessage(warn_text, reply=message_id)
+
+            if "username" in mode:
+                if chat_id in ban_username_chat and re.search(ban_username_regex, text):
+                    EC.deletemessage(chat_id, message_id)
+
+                    for ban_chat_id in ban_username_chat:
+                        EC.log("[spam_ban] kick {} in {} {}".format(user_id, ban_chat_id, text))
+
+                        url = "https://api.telegram.org/bot"+EC.token+"/kickChatMember?chat_id="+str(ban_chat_id)+"&user_id="+str(user_id)+"&until_date="+str(int(time.time()+86400*7))
+                        subprocess.Popen(['curl', '-s', url])
+
+                    message = '#封 #自動 所有群組(from {0}) ECbot banned <a href="tg://user?id={1}">{1}</a>\n理由：宣傳性用戶名'.format(
+                            groups_name[chat_id],
+                            user_id
+                        )
+                    EC.log("[spam_ban] message {}".format(message))
+                    EC.sendmessage(chat_id=log_chat_id, message=message, parse_mode="HTML")
+
+                elif chat_id in warn_username_chat and re.search(warn_username_regex, text):
+                    EC.log("[spam_ban] warn {} in {} {}".format(user_id, chat_id, text))
+                    EC.sendmessage(warn_text, reply=message_id)
 
         except Exception as e:
             traceback.print_exc()
