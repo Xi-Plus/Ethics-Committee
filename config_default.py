84cac59443f26ba73fd369441b0ec7c08827c6ed
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-03-31 16:16:49 +0800

    Update config_default.py

--- config_default.py
+++ config_default.py
@@ -2,3 +2,20 @@ from Kamisu66 import load_extensions
 
 extensions = []
 extensions.append(load_extensions('record')())
+
+cfg = {
+    'database': {
+        'host': 'localhost',
+        'user': '',
+        'passwd': '',
+        'db': 'EthicsCommittee',
+        'charset': '',
+    },
+    'telegram': {
+        'token': '',
+        'botname': '',
+        'botid': 0,
+        'url': '',
+        'max_connections': '5',
+    },
+}
