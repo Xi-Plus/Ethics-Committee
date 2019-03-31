7834495b1c03f78a27b960210679f993f196c4dc
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-03-31 16:18:12 +0800

    Create bot_added_notification.py

---
+++ extensions/bot_added_notification.py
@@ -0,0 +1,33 @@
+import telegram
+
+from Kamisu66 import EthicsCommitteeExtension
+
+
+class Bot_added_notification(EthicsCommitteeExtension):
+    def __init__(self, notice_chat_id):
+        self.notice_chat_id = notice_chat_id
+
+    def main(self, EC):
+        if EC.update.message.new_chat_members and EC.update.message.new_chat_members[0].id == EC.bot.id:
+            message = '機器人被 <a href="tg://user?id={}">{}</a> 加入了群 {} {}'.format(
+                EC.update.effective_user.id,
+                EC.update.effective_user.full_name,
+                EC.update.effective_chat.id,
+                EC.update.effective_chat.title)
+            EC.bot.send_message(chat_id=self.notice_chat_id,
+                                text=message,
+                                parse_mode=telegram.ParseMode.HTML)
+
+        if EC.update.message.left_chat_member and EC.update.message.left_chat_member.id == EC.bot.id:
+            message = '機器人被 <a href="tg://user?id={}">{}</a> 移出了群 {} {}'.format(
+                EC.update.effective_user.id,
+                EC.update.effective_user.full_name,
+                EC.update.effective_chat.id,
+                EC.update.effective_chat.title)
+            EC.bot.send_message(chat_id=self.notice_chat_id,
+                                text=message,
+                                parse_mode=telegram.ParseMode.HTML)
+
+
+def __mainclass__():
+    return Bot_added_notification
