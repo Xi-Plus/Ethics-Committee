d2217953e9342e57761d1a3886761a122e3179d8
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-03 10:36:08 +0800

    spam_ban: Move func to class level

--- extensions/spam_ban.py
+++ extensions/spam_ban.py
@@ -1,6 +1,4 @@
 import argparse
-import contextlib
-import io
 import json
 import re
 import shlex
@@ -19,15 +17,22 @@ PERMISSION_GRANT = MODULE_NAME + '_grant'
 
 
 class Spam_ban(EthicsCommitteeExtension):
+    EC = None
+    chat_id = None
+    user_id = None
+    textnorm = None
+    message_deleted = False
+
     def main(self, EC):
+        self.EC = EC
         data = EC.data
         if "message" in data or "edited_message" in data:
             if "message" in data:
                 message = data["message"]
             elif "edited_message" in data:
                 message = data["edited_message"]
-            chat_id = message["chat"]["id"]
-            user_id = message["from"]["id"]
+            self.chat_id = chat_id = message["chat"]["id"]
+            self.user_id = user_id = message["from"]["id"]
 
             if chat_id not in (global_ban_chat + test_chat + global_ban_cmd_chat):
                 return
@@ -72,6 +77,7 @@ class Spam_ban(EthicsCommitteeExtension):
                 textnorm = Equivset(text)
                 # EC.log("[spam_ban] Equivset ok {}".format(text2))
                 textnorm = text + "\n" + textnorm
+                self.textnorm = textnorm
             except Exception:
                 # EC.log("[spam_ban] Equivset fail {}".format(text))
                 EC.log(traceback.format_exc())
@@ -86,123 +92,6 @@ class Spam_ban(EthicsCommitteeExtension):
                     user_msg_cnt = 0
                 else:
                     user_msg_cnt = int(rows[0][0])
-
-                message_deleted = False
-
-                # action list start
-                def action_ban_all_chat(user_id, duration=604800):
-                    EC.log("[spam_ban] kick {} in {}".format(
-                        user_id, ", ".join(map(str, global_ban_chat))))
-                    for ban_chat_id in global_ban_chat:
-                        url = "https://api.telegram.org/bot" + EC.token + "/kickChatMember?chat_id=" + \
-                            str(ban_chat_id) + "&user_id=" + str(user_id) + \
-                            "&until_date=" + str(int(time.time() + duration))
-                        subprocess.Popen(['curl', '-s', url])
-
-                def action_unban_all_chat(user_id):
-                    EC.log("[spam_ban] unban {} in {}".format(
-                        user_id, ", ".join(map(str, global_ban_chat))))
-                    for ban_chat_id in global_ban_chat:
-                        url = "https://api.telegram.org/bot{0}/unbanChatMember?chat_id={1}&user_id={2}".format(
-                            EC.token, ban_chat_id, user_id)
-                        subprocess.Popen(['curl', '-s', url])
-
-                def action_del_all_msg(user_id):
-                    global message_deleted
-                    message_deleted = True
-
-                    EC.cur.execute("""SELECT `chat_id`, `message_id`, `type` FROM `message` WHERE `user_id` = %s AND `date` > %s""", (user_id, int(
-                        time.time() - delete_limit)))
-                    rows = EC.cur.fetchall()
-                    EC.log(
-                        "[spam_ban] find {} messages to delete".format(len(rows)))
-                    for row in rows:
-                        if int(row[0]) in global_ban_chat:
-                            EC.log("[spam_ban] delete {} ({}) in {}".format(
-                                row[1], row[2], row[0]))
-                            EC.deletemessage(row[0], row[1])
-
-                def action_warn(message_id=None, text=warn_text):
-                    global message_deleted
-                    if message_deleted:
-                        EC.log("[spam_ban] warn {} in {} {} skiped, msg deleted.".format(
-                            user_id, chat_id, textnorm))
-                        return
-                    message_deleted = True
-
-                    EC.log("[spam_ban] warn {} in {} {}".format(
-                        user_id, chat_id, textnorm))
-                    EC.sendmessage(text, reply=message_id)
-                    EC.sendmessage('/globalban {}'.format(user_id))
-
-                def action_log_admin(hashtag, admin_user_id, admin_name, action, ban_user_id, reason, duration):
-                    message = '{0} 所有群組 <a href="tg://user?id={1}">{2}</a> via ECbot {3} <a href="tg://user?id={4}">{4}</a> 期限為{6}\n理由：{5}'.format(
-                        hashtag,
-                        admin_user_id,
-                        admin_name,
-                        action,
-                        ban_user_id,
-                        reason,
-                        duration
-                    )
-                    EC.log("[spam_ban] message {}".format(message))
-                    EC.sendmessage(chat_id=log_chat_id,
-                                   message=message, parse_mode="HTML")
-
-                def action_log_bot(ban_user_id, reason, duration):
-                    message = '#封 #自動 所有群組(from {0}) ECbot banned <a href="tg://user?id={1}">{1}</a> 期限為{3}\n理由：{2}'.format(
-                        groups_name[chat_id],
-                        ban_user_id,
-                        reason,
-                        duration
-                    )
-                    EC.log("[spam_ban] message {}".format(message))
-                    EC.sendmessage(chat_id=log_chat_id,
-                                   message=message, parse_mode="HTML")
-
-                # action list end
-                # function start
-
-                def parse_duration(duration):
-                    duration = duration.lower()
-                    if duration in ['inf', 'indef', 'infinite', 'infinity', 'indefinite', 'never']:
-                        return 0
-                    m = re.search(r'^(\d+)$', duration)
-                    if m:
-                        return int(duration)
-                    m = re.search(r'^(\d+)(s|min|h|d|w|m)$', duration)
-                    if m:
-                        number = int(m.group(1))
-                        unit = m.group(2)
-                        multiple = {
-                            's': 1,
-                            'min': 60,
-                            'h': 60 * 60,
-                            'd': 60 * 60 * 24,
-                            'w': 60 * 60 * 24 * 7,
-                            'm': 60 * 60 * 24 * 30
-                        }
-                        return number * multiple[unit]
-                    return None
-
-                def duration_text(duration):
-                    if duration == 0:
-                        return '永久'
-                    res = ""
-                    if duration // (60 * 60 * 24) >= 1:
-                        res += '{}日'.format(int(duration // (60 * 60 * 24)))
-                        duration %= 60 * 60 * 24
-                    if duration // (60 * 60) >= 1:
-                        res += '{}小時'.format(int(duration // (60 * 60)))
-                        duration %= 60 * 60
-                    if duration // 60 >= 1:
-                        res += '{}分'.format(int(duration // 60))
-                        duration %= 60
-                    if duration >= 1:
-                        res += '{}秒'.format(int(duration))
-                    return res
-
-                # function end
 
                 if 'text' in mode and text.startswith('/') and chat_id in global_ban_cmd_chat:
                     cmd = shlex.split(text)
@@ -234,29 +123,29 @@ class Spam_ban(EthicsCommitteeExtension):
                                         '需要回覆訊息或用參數指定封鎖目標', reply=message_id)
                                 else:
                                     reason = args.r
-                                    duration = parse_duration(args.d)
+                                    duration = self.parse_duration(args.d)
                                     if duration is None:
                                         EC.sendmessage(
                                             '指定的時長無效', reply=message_id)
 
                                     elif action == "globalban":
-                                        action_ban_all_chat(
+                                        self.action_ban_all_chat(
                                             ban_user_id, duration)
-                                        action_del_all_msg(ban_user_id)
-                                        action_log_admin(
+                                        self.action_del_all_msg(ban_user_id)
+                                        self.action_log_admin(
                                             '#封', user_id,
                                             message["from"]["first_name"],
                                             'banned', ban_user_id, reason,
-                                            duration_text(duration)
+                                            self.duration_text(duration)
                                         )
 
                                     elif action == "globalunban":
-                                        action_unban_all_chat(ban_user_id)
-                                        action_log_admin(
+                                        self.action_unban_all_chat(ban_user_id)
+                                        self.action_log_admin(
                                             '#解', user_id,
                                             message["from"]["first_name"],
                                             'unbanned', ban_user_id, reason,
-                                            duration_text(duration)
+                                            self.duration_text(duration)
                                         )
 
                                     EC.deletemessage(chat_id, message_id)
@@ -305,13 +194,13 @@ class Spam_ban(EthicsCommitteeExtension):
                 if "text" in mode:
                     if user_msg_cnt <= 5:
                         if chat_id in ban_text_chat and re.search(ban_text_regex, textnorm, flags=re.I):
-                            action_ban_all_chat(user_id, 604800)
-                            action_del_all_msg(user_id)
-                            action_log_bot(user_id, '宣傳文字',
-                                           duration_text(604800))
+                            self.action_ban_all_chat(user_id, 604800)
+                            self.action_del_all_msg(user_id)
+                            self.action_log_bot(user_id, '宣傳文字',
+                                                self.duration_text(604800))
 
                         elif chat_id in warn_text_chat and re.search(warn_text_regex, textnorm, flags=re.I):
-                            action_warn(message_id)
+                            self.action_warn(message_id)
 
                     if chat_id in test_chat and re.search(r'/test', text):
                         spam_type = []
@@ -332,33 +221,144 @@ class Spam_ban(EthicsCommitteeExtension):
 
                 if "username" in mode:
                     if chat_id in ban_username_chat and re.search(ban_username_regex, textnorm, flags=re.I):
-                        action_ban_all_chat(user_id, 604800)
-                        action_del_all_msg(user_id)
-                        action_log_bot(user_id, '宣傳性用戶名',
-                                       duration_text(604800))
+                        self.action_ban_all_chat(user_id, 604800)
+                        self.action_del_all_msg(user_id)
+                        self.action_log_bot(user_id, '宣傳性用戶名',
+                                            self.duration_text(604800))
 
                     elif chat_id in warn_username_chat and re.search(warn_username_regex, textnorm, flags=re.I):
-                        action_warn(message_id)
+                        self.action_warn(message_id)
 
                 if "photo" in mode:
                     if chat_id in ban_photo_chat:
                         if user_msg_cnt <= 5:
-                            action_ban_all_chat(user_id, 604800)
-                            action_log_bot(
-                                user_id, '發送圖片', duration_text(604800))
+                            self.action_ban_all_chat(user_id, 604800)
+                            self.action_log_bot(
+                                user_id, '發送圖片', self.duration_text(604800))
 
-                if "forward" in mode and not message_deleted:
+                if "forward" in mode and not self.message_deleted:
                     if user_msg_cnt <= 5:
                         EC.sendmessage('https://t.me/{}/{}'.format(
                             message["chat"]["username"], message_id), chat_id=warn_forward_chat_id, parse_mode="HTML")
                         EC.log("[spam_ban] forward {}".format(
                             json.dumps(message)))
                         if "forward_from_chat" in message and message["forward_from_message_id"] < 10:
-                            action_warn(message_id)
+                            self.action_warn(message_id)
 
             except Exception:
                 traceback.print_exc()
                 EC.log("[spam_ban] " + traceback.format_exc())
+
+    # action list start
+    def action_ban_all_chat(self, user_id, duration=604800):
+        self.EC.log("[spam_ban] kick {} in {}".format(
+            user_id, ", ".join(map(str, global_ban_chat))))
+        for ban_chat_id in global_ban_chat:
+            url = "https://api.telegram.org/bot" + self.EC.token + "/kickChatMember?chat_id=" + \
+                str(ban_chat_id) + "&user_id=" + str(user_id) + \
+                "&until_date=" + str(int(time.time() + duration))
+            subprocess.Popen(['curl', '-s', url])
+
+    def action_unban_all_chat(self, user_id):
+        self.EC.log("[spam_ban] unban {} in {}".format(
+            user_id, ", ".join(map(str, global_ban_chat))))
+        for ban_chat_id in global_ban_chat:
+            url = "https://api.telegram.org/bot{0}/unbanChatMember?chat_id={1}&user_id={2}".format(
+                self.EC.token, ban_chat_id, user_id)
+            subprocess.Popen(['curl', '-s', url])
+
+    def action_del_all_msg(self, user_id):
+        self.message_deleted = True
+
+        self.EC.cur.execute("""SELECT `chat_id`, `message_id`, `type` FROM `message` WHERE `user_id` = %s AND `date` > %s""", (user_id, int(
+            time.time() - delete_limit)))
+        rows = self.EC.cur.fetchall()
+        self.EC.log(
+            "[spam_ban] find {} messages to delete".format(len(rows)))
+        for row in rows:
+            if int(row[0]) in global_ban_chat:
+                self.EC.log("[spam_ban] delete {} ({}) in {}".format(
+                    row[1], row[2], row[0]))
+                self.EC.deletemessage(row[0], row[1])
+
+    def action_warn(self, message_id=None, text=warn_text):
+        if self.message_deleted:
+            self.EC.log("[spam_ban] warn {} in {} {} skiped, msg deleted.".format(
+                self.user_id, self.chat_id, self.textnorm))
+            return
+        self.message_deleted = True
+
+        self.EC.log("[spam_ban] warn {} in {} {}".format(
+            self.user_id, self.chat_id, self.textnorm))
+        self.EC.sendmessage(text, reply=message_id)
+        self.EC.sendmessage('/globalban {}'.format(self.user_id))
+
+    def action_log_admin(self, hashtag, admin_user_id, admin_name, action, ban_user_id, reason, duration):
+        message = '{0} 所有群組 <a href="tg://user?id={1}">{2}</a> via ECbot {3} <a href="tg://user?id={4}">{4}</a> 期限為{6}\n理由：{5}'.format(
+            hashtag,
+            admin_user_id,
+            admin_name,
+            action,
+            ban_user_id,
+            reason,
+            duration
+        )
+        self.EC.log("[spam_ban] message {}".format(message))
+        self.EC.sendmessage(chat_id=log_chat_id,
+                            message=message, parse_mode="HTML")
+
+    def action_log_bot(self, ban_user_id, reason, duration):
+        message = '#封 #自動 所有群組(from {0}) ECbot banned <a href="tg://user?id={1}">{1}</a> 期限為{3}\n理由：{2}'.format(
+            groups_name[self.chat_id],
+            ban_user_id,
+            reason,
+            duration
+        )
+        self.EC.log("[spam_ban] message {}".format(message))
+        self.EC.sendmessage(chat_id=log_chat_id,
+                            message=message, parse_mode="HTML")
+    # action list end
+
+    # function start
+    def parse_duration(self, duration):
+        duration = duration.lower()
+        if duration in ['inf', 'indef', 'infinite', 'infinity', 'indefinite', 'never']:
+            return 0
+        m = re.search(r'^(\d+)$', duration)
+        if m:
+            return int(duration)
+        m = re.search(r'^(\d+)(s|min|h|d|w|m)$', duration)
+        if m:
+            number = int(m.group(1))
+            unit = m.group(2)
+            multiple = {
+                's': 1,
+                'min': 60,
+                'h': 60 * 60,
+                'd': 60 * 60 * 24,
+                'w': 60 * 60 * 24 * 7,
+                'm': 60 * 60 * 24 * 30
+            }
+            return number * multiple[unit]
+        return None
+
+    def duration_text(self, duration):
+        if duration == 0:
+            return '永久'
+        res = ""
+        if duration // (60 * 60 * 24) >= 1:
+            res += '{}日'.format(int(duration // (60 * 60 * 24)))
+            duration %= 60 * 60 * 24
+        if duration // (60 * 60) >= 1:
+            res += '{}小時'.format(int(duration // (60 * 60)))
+            duration %= 60 * 60
+        if duration // 60 >= 1:
+            res += '{}分'.format(int(duration // 60))
+            duration %= 60
+        if duration >= 1:
+            res += '{}秒'.format(int(duration))
+        return res
+    # function end
 
     def web(self):
         EC = EthicsCommittee(MODULE_NAME + '_web', MODULE_NAME + '_web')
