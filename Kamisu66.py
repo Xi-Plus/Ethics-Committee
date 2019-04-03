4257d5612835d3b237a10fe97acb6f5c1e0c4c2d
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-03 19:57:26 +0800

    Kamisu66: Fix self.chat_id

--- Kamisu66.py
+++ Kamisu66.py
@@ -34,7 +34,7 @@ class EthicsCommittee:
         else:
             self.data = update
             self.update = telegram.Update.de_json(update, self.bot)
-            self.chat_id = self.update.effective_user.id
+            self.chat_id = self.update.effective_chat.id
             self.user_id = self.update.effective_user.id
 
     def sendmessage(self, message, parse_mode="Markdown", reply=False, reply_markup=None, chat_id=None):
