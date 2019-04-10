1f88b195934e35701f0cef1468afc3c17a47b356
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-10 19:30:57 +0800

    change_invite_link: Add option new-message

--- extensions/change_invite_link.py
+++ extensions/change_invite_link.py
@@ -13,40 +13,42 @@ from Kamisu66 import EthicsCommittee
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('chat_id', type=int)
+parser.add_argument('group')
 parser.add_argument('--no-message', dest='message', action='store_false')
-parser.set_defaults(message=True)
+parser.add_argument('--new-message', dest='new', action='store_true')
+parser.set_defaults(message=True, new=False)
 args = parser.parse_args()
 print(args)
 
-chat_id = args.chat_id
-
 EC = EthicsCommittee(0, 0)
 
-if chat_id not in change_groups:
-    exit('chat_id not found')
+if args.group not in change_groups:
+    exit('chat not found')
 
-group = change_groups[chat_id]
+group = change_groups[args.group]
 
 print(group)
 
-link = EC.bot.export_chat_invite_link(chat_id=chat_id)
+link = EC.bot.export_chat_invite_link(chat_id=group['chat_id'])
 
-text = group['message']['text'].format(link)
+if args.message:
+    text = group['message']['text'].format(link)
+else:
+    text = group['message']['no_text'].format(link)
 
 print(text)
 
-if args.message:
-    try:
-        res = EC.bot.edit_message_text(
-            chat_id=group['message']['chat_id'],
-            message_id=group['message']['message_id'],
-            text=text,
-            parse_mode='',
-        )
-        print(res)
-    except telegram.TelegramError as e:
-        print(e.message)
+try:
+    res = EC.bot.edit_message_text(
+        chat_id=group['message']['chat_id'],
+        message_id=group['message']['message_id'],
+        text=text,
+        parse_mode='',
+    )
+    print(res)
+except telegram.TelegramError as e:
+    print(e.message)
+    if args.new:
         res = EC.bot.send_message(
             chat_id=group['message']['chat_id'],
             message_id=group['message']['message_id'],
