1a3e1f77a134478cebb2f2593d96b4d40d82a91c
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-10 10:10:58 +0800

    change_invite_link: Use telegram module

--- extensions/change_invite_link.py
+++ extensions/change_invite_link.py
@@ -1,50 +1,56 @@
 # -*- coding: utf-8 -*-
-import sys
-import os
-sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__))+"/../"))
+import argparse
 import json
-import configparser
-from Kamisu66 import *
-import time
-import urllib.request
+import os
+import sys
+
+import telegram
+
 from change_invite_link_config import *
+sys.path.insert(0, os.path.realpath(
+    os.path.dirname(os.path.realpath(__file__)) + '/../'))
+from Kamisu66 import EthicsCommittee
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('chat_id', type=int)
+parser.add_argument('--no-message', dest='message', action='store_false')
+parser.set_defaults(message=True)
+args = parser.parse_args()
+print(args)
+
+chat_id = args.chat_id
 
 EC = EthicsCommittee(0, 0)
 
-if len(sys.argv) < 2:
-	exit("not given chat_id")
-
-try:
-	chat_id = int(sys.argv[1])
-except ValueError as e:
-	exit("chat_id not a value")
-
 if chat_id not in change_groups:
-	exit("chat_id not found")
+    exit('chat_id not found')
 
 group = change_groups[chat_id]
 
 print(group)
 
-url = "https://api.telegram.org/bot"+EC.token+"/exportChatInviteLink?chat_id="+str(chat_id)
+link = EC.bot.export_chat_invite_link(chat_id=chat_id)
 
-try:
-	res = urllib.request.urlopen(url).read().decode("utf8")
-except urllib.error.HTTPError as e:
-	exit(e.read().decode("utf8"))
-
-res = json.loads(res)
-
-link = res["result"]
-text = group["message"]["text"].format(link)
+text = group['message']['text'].format(link)
 
 print(text)
 
-res = EC.editmessage(group["message"]["message_id"], text,
-                     chat_id=group["message"]["chat_id"], parse_mode="")
-print(res)
-
-if res['ok'] is False and res['description'] == 'Bad Request: message to edit not found':
-    res = EC.sendmessage(text,
-                         chat_id=group["message"]["chat_id"], parse_mode="")
-    print(res)
+if args.message:
+    try:
+        res = EC.bot.edit_message_text(
+            chat_id=group['message']['chat_id'],
+            message_id=group['message']['message_id'],
+            text=text,
+            parse_mode='',
+        )
+        print(res)
+    except telegram.TelegramError as e:
+        print(e.message)
+        res = EC.bot.send_message(
+            chat_id=group['message']['chat_id'],
+            message_id=group['message']['message_id'],
+            text=text,
+            parse_mode='',
+        )
+        print(res)
