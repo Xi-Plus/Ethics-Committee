7aa63e00750b8611def1c9cb224bf0fbf592a029
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-05-14 10:07:41 +0800

    Create userinfo
    Close #1

---
+++ extensions/userinfo.py
@@ -0,0 +1,26 @@
+import telegram
+
+from Kamisu66 import EthicsCommitteeExtension
+
+
+class RemoveKeyboard(EthicsCommitteeExtension):
+    def main(self, EC):
+        if not EC.update.message:
+            return
+
+        if not EC.update.message.reply_to_message:
+            return
+
+        if EC.update.message.text == '/getid':
+            response = '<a href="tg://user?id={0}">{0}</a>'.format(
+                EC.update.message.reply_to_message.from_user.id
+            )
+            message = EC.update.message.reply_text(
+                text=response,
+                quote=True,
+                parse_mode=telegram.ParseMode.HTML,
+            )
+
+
+def __mainclass__():
+    return RemoveKeyboard
