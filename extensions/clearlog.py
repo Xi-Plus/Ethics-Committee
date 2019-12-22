e7d5d46ba51f8ac06b2c80d42c8e707fd9364d74
xiplus <huangxuanyuxiplus@gmail.com>
2019-12-22 12:56:11 +0800

    Clear more tables

--- extensions/clearlog.py
+++ extensions/clearlog.py
@@ -4,7 +4,7 @@ import os
 import sys
 import time
 
-from clearlog_config import limit  # pylint: disable=E0401
+from clearlog_config import limit, tables  # pylint: disable=E0401
 sys.path.insert(0, os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../"))
 from Kamisu66 import EthicsCommittee
 
@@ -13,6 +13,7 @@ EC = EthicsCommittee(0, 0)
 print(limit)
 timestamp = datetime.datetime.fromtimestamp(int(time.time() - limit)).strftime('%Y-%m-%d %H:%M:%S')
 
-rows = EC.cur.execute("""DELETE FROM log WHERE `time` < %s""", (timestamp))
-EC.db.commit()
-print(f"delete {rows} rows")
+for table in tables:
+    rows = EC.cur.execute("""DELETE FROM {} WHERE `time` < %s""".format(table), (timestamp))
+    EC.db.commit()
+    print("delete {} rows from {} table".format(rows, table))
