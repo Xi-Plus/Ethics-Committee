7aa020037469f5b3c619d9aa2113e9faf30932e4
xiplus <huangxuanyuxiplus@gmail.com>
2020-03-25 20:23:30 +0800

    Fix bug

--- extensions/common_groups_info.py
+++ extensions/common_groups_info.py
@@ -50,7 +50,7 @@ class CommonGroupsInfo(EthicsCommitteeExtension):  # pylint: disable=W0223
 
         if EC.update.message.new_chat_members:
             if self.setting[chat_id]['joincheck']:
-                user_id = EC.update.effective_chat.id
+                user_id = EC.update.effective_user.id
                 response = self._check_user(EC, chat_id, user_id)
 
         elif EC.update.message.text:
