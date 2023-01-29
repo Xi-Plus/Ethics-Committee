1c915ee9d950aaf6f5aa00fb356d66dd21e7cc32
Xi-Plus <huangxuanyuxiplus@gmail.com>
2023-01-29 12:14:54 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,4 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -26,7 +25,7 @@ CREATE TABLE `admins` (
   `can_send_media_messages` tinyint(4) NOT NULL,
   `can_send_messages` tinyint(4) NOT NULL,
   `can_send_other_messages` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 DELIMITER $$
 CREATE TRIGGER `admins_add` AFTER INSERT ON `admins` FOR EACH ROW INSERT INTO `admins_changes` (`chat_id`, `user_id`, `action`, `can_add_web_page_previews`, `can_be_edited`, `can_change_info`, `can_delete_messages`, `can_edit_messages`, `can_invite_users`, `can_pin_messages`, `can_post_messages`, `can_promote_members`, `can_restrict_members`, `can_send_media_messages`, `can_send_messages`, `can_send_other_messages`) VALUES (
 	NEW.`chat_id`, NEW.`user_id`, 'add',
@@ -91,55 +90,55 @@ DELIMITER ;
 CREATE TABLE `admins_changes` (
   `chat_id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
-  `action` varchar(10) COLLATE utf8_bin NOT NULL,
-  `can_add_web_page_previews` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_be_edited` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_change_info` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_delete_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_edit_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_invite_users` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_pin_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_post_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_promote_members` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_restrict_members` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_send_media_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_send_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `can_send_other_messages` varchar(5) COLLATE utf8_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `action` varchar(10) NOT NULL,
+  `can_add_web_page_previews` varchar(5) NOT NULL,
+  `can_be_edited` varchar(5) NOT NULL,
+  `can_change_info` varchar(5) NOT NULL,
+  `can_delete_messages` varchar(5) NOT NULL,
+  `can_edit_messages` varchar(5) NOT NULL,
+  `can_invite_users` varchar(5) NOT NULL,
+  `can_pin_messages` varchar(5) NOT NULL,
+  `can_post_messages` varchar(5) NOT NULL,
+  `can_promote_members` varchar(5) NOT NULL,
+  `can_restrict_members` varchar(5) NOT NULL,
+  `can_send_media_messages` varchar(5) NOT NULL,
+  `can_send_messages` varchar(5) NOT NULL,
+  `can_send_other_messages` varchar(5) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `group_name` (
   `chat_id` bigint(20) NOT NULL,
-  `title` varchar(255) COLLATE utf8_bin NOT NULL,
-  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `title` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `group_setting` (
   `chat_id` bigint(20) DEFAULT NULL,
-  `key` varchar(64) COLLATE utf8_bin NOT NULL,
+  `key` varchar(64) NOT NULL,
   `value` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `log` (
   `log_id` int(11) NOT NULL,
   `chat_id` bigint(20) NOT NULL,
   `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `message` (
   `chat_id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `message_id` int(11) NOT NULL,
   `full_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `type` varchar(20) COLLATE utf8_bin NOT NULL,
+  `type` varchar(20) NOT NULL,
   `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `date` int(11) NOT NULL,
   `reply_to_message_id` int(11) DEFAULT NULL,
   `reply_to_user_id` bigint(20) DEFAULT NULL,
-  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `time` timestamp NULL DEFAULT current_timestamp(),
+  `deleted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 DELIMITER $$
 CREATE TRIGGER `count` AFTER INSERT ON `message` FOR EACH ROW BEGIN
 INSERT INTO `message_count` (`chat_id`, `user_id`, `type`, `count`) VALUES (NEW.chat_id, NEW.user_id, NEW.type, 1)
@@ -153,17 +152,17 @@ DELIMITER ;
 CREATE TABLE `message_count` (
   `chat_id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
-  `type` varchar(20) COLLATE utf8_bin NOT NULL,
-  `count` int(11) NOT NULL DEFAULT '0',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `type` varchar(20) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `permissions` (
   `chat_id` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) NOT NULL,
-  `user_right` varchar(50) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `user_right` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 DELIMITER $$
 CREATE TRIGGER `permissions_change_add` AFTER INSERT ON `permissions` FOR EACH ROW INSERT INTO `permissions_changes`
 (`chat_id`, `user_id`, `action`, `user_right`) VALUES (NEW.chat_id, NEW.user_id, 'add', NEW.user_right)
@@ -178,17 +177,17 @@ DELIMITER ;
 CREATE TABLE `permissions_changes` (
   `chat_id` bigint(20) DEFAULT NULL,
   `user_id` bigint(20) NOT NULL,
-  `action` varchar(10) COLLATE utf8_bin NOT NULL,
-  `user_right` varchar(50) COLLATE utf8_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `action` varchar(10) NOT NULL,
+  `user_right` varchar(50) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 CREATE TABLE `user_name` (
   `user_id` bigint(20) NOT NULL,
   `full_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL,
-  `comment` text COLLATE utf8_bin
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `username` varchar(255) DEFAULT NULL,
+  `comment` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 DELIMITER $$
 CREATE TRIGGER `change_user_name` AFTER UPDATE ON `user_name` FOR EACH ROW BEGIN
 INSERT INTO `user_name_changes` (`user_id`, `old_name`, `new_name`) VALUES (NEW.user_id, CONCAT( OLD.full_name," (",IFNULL(OLD.username, ''), ")"), CONCAT( NEW.full_name," (",IFNULL(NEW.username, ''), ")"));
@@ -215,12 +214,15 @@ CREATE TABLE `user_name_changes` (
   `user_id` bigint(20) NOT NULL,
   `old_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `new_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin;
 
 
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`chat_id`,`user_id`);
+
+ALTER TABLE `admins_changes`
+  ADD KEY `time` (`time`);
 
 ALTER TABLE `group_name`
   ADD PRIMARY KEY (`chat_id`);
@@ -231,14 +233,21 @@ ALTER TABLE `group_setting`
 ALTER TABLE `log`
   ADD PRIMARY KEY (`log_id`);
 
+ALTER TABLE `message`
+  ADD KEY `date` (`date`);
+
 ALTER TABLE `message_count`
   ADD PRIMARY KEY (`chat_id`,`user_id`,`type`);
 
 ALTER TABLE `permissions`
-  ADD KEY `permissions_ibfk_1` (`chat_id`);
+  ADD KEY `permissions_ibfk_1` (`chat_id`),
+  ADD KEY `user_id` (`user_id`);
 
 ALTER TABLE `user_name`
   ADD PRIMARY KEY (`user_id`);
+
+ALTER TABLE `user_name_changes`
+  ADD KEY `time` (`time`);
 
 
 ALTER TABLE `log`
